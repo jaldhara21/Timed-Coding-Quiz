b5d937636f10b215c04e478754e5df43ce79ce21
jaldhara.patel <jaldharapatel91@gmail.com>
2023-05-09 14:15:31 -0400

    format all files

--- assets/logic.js
+++ assets/logic.js
@@ -22,168 +22,164 @@ var shuffledQuestions, currentQuestionIndex;
 // Attach event listener to start button to call startgame function on click and next button.
 startButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
+  currentQuestionIndex++;
+  setNextQuestion();
 });
 
 //Create a function for timer countdown
 function timerCount() {
-    timeLeft--;
-    timerEl.textContent = "Time: " + timeLeft;
-    if (timeLeft <= 0) {
-        saveScore();
-    }
-};
+  timeLeft--;
+  timerEl.textContent = "Time: " + timeLeft;
+  if (timeLeft <= 0) {
+    saveScore();
+  }
+}
 
 // Create a startGame function to start quiz
 function startGame() {
-    timerID = setInterval(timerCount, 1000);
-    startContainerEl.classList.add("hide");
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerEl.classList.remove("hide");
+  timerID = setInterval(timerCount, 1000);
+  startContainerEl.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerEl.classList.remove("hide");
 
-    // Timer will start as soon as start button is clicked on the screen
-    timerCount();
-    setNextQuestion();
-};
+  // Timer will start as soon as start button is clicked on the screen
+  timerCount();
+  setNextQuestion();
+}
 
 // Creat a function for Go to the next question
 function setNextQuestion() {
-    resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-};
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
 
 // Create function to Display question and answers
 function showQuestion(question) {
-    console.log(question)
-    questionEl.innerText = question.question
-    question.answers.forEach(answer => {
-        var button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
-        // Add a data attribute to the button to check it's the correct answer or not
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsEl.appendChild(button)
-    })
-}; 
+  console.log(question);
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    // Add a data attribute to the button to check it's the correct answer or not
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsEl.appendChild(button);
+  });
+}
 
 // Reset state function create
 function resetState() {
-    //clearStatusClass(document.body)
-    console.log('next btn: ', nextButton, checkAnswerEl);
-    nextButton.classList.add("hide");
-    checkAnswerEl.classList.add("hide");
-    while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild
-            (answerButtonsEl.firstChild)
-    }
-};
+  //clearStatusClass(document.body)
+  console.log("next btn: ", nextButton, checkAnswerEl);
+  nextButton.classList.add("hide");
+  checkAnswerEl.classList.add("hide");
+  while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+  }
+}
 
 // Select answer button function
 function selectAnswer(e) {
-    var selectedButton = e.target;
-    //console.dir(selectedButton);
-    var correct = selectedButton.dataset.correct;
-    checkAnswerEl.classList.remove("hide")
-    // Create if else statement to Check if the answer is right or wrong then show the text
-    if (correct) {
-        checkAnswerEl.innerHTML = "You got it right!";
+  var selectedButton = e.target;
+  //console.dir(selectedButton);
+  var correct = selectedButton.dataset.correct;
+  checkAnswerEl.classList.remove("hide");
+  // Create if else statement to Check if the answer is right or wrong then show the text
+  if (correct) {
+    checkAnswerEl.innerHTML = "You got it right!";
+  } else {
+    checkAnswerEl.innerHTML = "Sorry that was not the correct answer.";
+    if (timeLeft <= 10) {
+      timeLeft = 0;
     } else {
-        checkAnswerEl.innerHTML = "Sorry that was not the correct answer.";
-        if (timeLeft <= 10) {
-            timeLeft = 0;
-        } else {
-            // If the answer is wrong, decrease time by 10
-            timeLeft -= 10;
-        }
+      // If the answer is wrong, decrease time by 10
+      timeLeft -= 10;
     }
-   
-    Array.from(answerButtonsEl.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
+  }
 
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove("hide")
-        checkAnswerEl.classList.remove("hide")
-    } else {
-        startButton.classList.remove("hide")
-        saveScore();
-    }
-};
+  Array.from(answerButtonsEl.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
 
-// Check and shows the correct answer by setting the color buttons 
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+    checkAnswerEl.classList.remove("hide");
+  } else {
+    startButton.classList.remove("hide");
+    saveScore();
+  }
+}
+
+// Check and shows the correct answer by setting the color buttons
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add("correct");
-    } else {
-        element.classList.add("wrong");
-    }
-};
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
 
 //  Remove all the classes
 function clearStatusClass(element) {
-    element.classList.remove("correct");
-    element.classList.remove("wrong");
-};
-
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 // create function for Save scores
 function saveScore() {
-    clearInterval(timerID);
-    timerEl.textContent = "Time: " + timeLeft;
-    setTimeout(function () {
-        //localStorage.setItem("scores", JSON.stringify(scores));
-        questionContainerEl.classList.add("hide");
-        document.getElementById("score-container").classList.remove("hide");
-        document.getElementById("your-score").textContent = "Your final score is " + timeLeft;
-
-    }, 2000)
-};
+  clearInterval(timerID);
+  timerEl.textContent = "Time: " + timeLeft;
+  setTimeout(function () {
+    //localStorage.setItem("scores", JSON.stringify(scores));
+    questionContainerEl.classList.add("hide");
+    document.getElementById("score-container").classList.remove("hide");
+    document.getElementById("your-score").textContent =
+      "Your final score is " + timeLeft;
+  }, 2000);
+}
 
 var loadScores = function () {
-    
+  if (!saveScore) {
+    return false;
+  }
 
-    if (!saveScore) {
-        return false;
-    }
+  saveScore = JSON.parse(saveScore);
+  var initials = document.querySelector("#initials-field").value;
+  var newScore = {
+    score: timeLeft,
+    initials: initials,
+  };
+  saveScore.push(newScore);
+  console.log(saveScore);
 
-    
-    saveScore = JSON.parse(saveScore);
-    var initials = document.querySelector("#initials-field").value;
-    var newScore = {
-        score: timeLeft,
-        initials: initials
-    }
-    saveScore.push(newScore);
-    console.log(saveScore)
-
-    saveScore.forEach(score => {
-        initialsField.innerText = score.initials
-        scoreField.innerText = score.score
-    })
+  saveScore.forEach((score) => {
+    initialsField.innerText = score.initials;
+    scoreField.innerText = score.score;
+  });
 };
 
 //create a show high score function
 function showHighScores(initials) {
-    document.getElementById("highscores").classList.remove("hide")
-    document.getElementById("score-container").classList.add("hide");
-    startContainerEl.classList.add("hide");
-    questionContainerEl.classList.add("hide");
-    if (typeof initials == "string") {
-        var score = {
-            initials, timeLeft
-        }
-        scores.push(score)
+  document.getElementById("highscores").classList.remove("hide");
+  document.getElementById("score-container").classList.add("hide");
+  startContainerEl.classList.add("hide");
+  questionContainerEl.classList.add("hide");
+  if (typeof initials == "string") {
+    var score = {
+      initials,
+      timeLeft,
     };
+    scores.push(score);
+  }
 
- var highScoreEl = document.getElementById("highscore");
- highScoreEl.innerHTML = "";
- for (i = 0; i < scores.length; i++) {
+  var highScoreEl = document.getElementById("highscore");
+  highScoreEl.innerHTML = "";
+  for (i = 0; i < scores.length; i++) {
     var div1 = document.createElement("div");
     div1.setAttribute("class", "name-div");
     div1.innerText = scores[i].initials;
@@ -193,30 +189,28 @@ function showHighScores(initials) {
 
     highScoreEl.appendChild(div1);
     highScoreEl.appendChild(div2);
+  }
+
+  // The scores data will be store in client storage
+  localStorage.setItem("scores", JSON.stringify(scores));
 }
 
-// The scores data will be store in client storage
-localStorage.setItem("scores", JSON.stringify(scores));
-
-};
-
-//Attaches event listener to view high score 
+//Attaches event listener to view high score
 viewHighScores.addEventListener("click", showHighScores);
 
-
 submitButton.addEventListener("click", function (event) {
-    event.preventDefault()
-    var initials = document.querySelector("#initial-field").value;
-    showHighScores(initials);
+  event.preventDefault();
+  var initials = document.querySelector("#initial-field").value;
+  showHighScores(initials);
 });
 
 //Attaches event listener to go back button
 goBackButton.addEventListener("click", function () {
-    window.location.reload();
+  window.location.reload();
 });
 
 //Attaches event listener to high score button and clear local storage item
 clearHighScoreButton.addEventListener("click", function () {
-    localStorage.clear();
-    document.getElementById("highscore").innerHTML = "";
+  localStorage.clear();
+  document.getElementById("highscore").innerHTML = "";
 });
--- assets/questions.js
+++ assets/questions.js
@@ -1,51 +1,54 @@
-
-// Strore the number of questions answered correctly to the question functions. 
+// Strore the number of questions answered correctly to the question functions.
 // Creat a multidimensional array with inner array element.
 var questions = [
-    {
-        question:"Which of the following is not a primitive data type in JavaScript?",
-        answers: [
-            { text: "Number", correct: false},
-            { text: "String", correct: false},
-            { text: "Boolean", correct: false},
-            { text: "Object", correct: true}
-        ]
-    },
-    {
-      question: " Which of the following attribute can hold the JavaScript version?",
-      answers:[
-        { text: "LANGUAGE", correct: true},
-        { text: "SCRIPT", correct: false},
-        {text: "VERSION", correct: false},
-        {text: "None of the above", correct: false}
-      ] 
-    },
-    {
-        question: "What are variables used for in JavaScript Programs?",
-        answers:[
-            { text: " Storing numbers, dates, or other values", correct: true},
-            { text: "Varying randomly", correct: false},
-            {text: "Causing high-school algebra flashbacks", correct: false},
-            {text: "None of the above", correct: false}
-          ]
-    },
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        answers:[
-            { text: "<js>" , correct: false},
-            { text: "<scripting>", correct: false},
-            {text: "<script>", correct: true},
-            {text: "<javascript>", correct: false}
-          ]
-    },
-    {
-        question: "Which of the following best describes JavaScript?",
-        answers:[
-            { text: "a low-level programming language" , correct: false},
-            { text: "a scripting language precompiled in the browser", correct: false},
-            {text: "a compiled scripting language", correct: false},
-            {text: "an object-oriented scripting language", correct: true}
-          ]
-    },
+  {
+    question:
+      "Which of the following is not a primitive data type in JavaScript?",
+    answers: [
+      { text: "Number", correct: false },
+      { text: "String", correct: false },
+      { text: "Boolean", correct: false },
+      { text: "Object", correct: true },
+    ],
+  },
+  {
+    question:
+      " Which of the following attribute can hold the JavaScript version?",
+    answers: [
+      { text: "LANGUAGE", correct: true },
+      { text: "SCRIPT", correct: false },
+      { text: "VERSION", correct: false },
+      { text: "None of the above", correct: false },
+    ],
+  },
+  {
+    question: "What are variables used for in JavaScript Programs?",
+    answers: [
+      { text: " Storing numbers, dates, or other values", correct: true },
+      { text: "Varying randomly", correct: false },
+      { text: "Causing high-school algebra flashbacks", correct: false },
+      { text: "None of the above", correct: false },
+    ],
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<js>", correct: false },
+      { text: "<scripting>", correct: false },
+      { text: "<script>", correct: true },
+      { text: "<javascript>", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following best describes JavaScript?",
+    answers: [
+      { text: "a low-level programming language", correct: false },
+      {
+        text: "a scripting language precompiled in the browser",
+        correct: false,
+      },
+      { text: "a compiled scripting language", correct: false },
+      { text: "an object-oriented scripting language", correct: true },
+    ],
+  },
 ];
-
